4c62b803dd61c8488f10163ca1756792f76bcc10
Joako <electronicnation09@gmail.com>
2025-12-01 19:07:55 +0100

    añadiendo 2 achivos principales

---
+++ BotAuto2.py
@@ -0,0 +1,180 @@
+import requests
+from datetime import datetime, timezone, timedelta
+import time
+import traceback
+import json
+
+utc_minus_8 = timezone(timedelta(hours=-8))
+
+headers = {
+    'accept': '*/*',
+    'accept-language': 'es-ES,es;q=0.9,en;q=0.8',
+    'origin': 'https://casinoscores.com',
+    'priority': 'u=1, i',
+    'referer': 'https://casinoscores.com/',
+    'sec-ch-ua': '"Chromium";v="142", "Google Chrome";v="142", "Not_A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36',
+}
+
+
+params = {
+    'page': 0,
+    'size': 16,
+    'sort': 'data.settledAt,desc',
+    'duration': 10,
+    'isMatched': 'false',
+    'wheelResults': 'LilBlues,BigOranges,HugeReds,Leaf1,Leaf2'
+
+}
+
+def telegram(mensaje, imagen):
+
+
+    CHANNEL_ID = -1002480092493
+    TOKEN = "8312889768:AAGtL6AYKolSoADt3L5c3NeNECKid8RrRew"
+    URL = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+
+    files = {'photo': open(f'{imagen}.png', 'rb')}
+
+
+    contenido = {'inline_keyboard': [
+
+        [{'text': 'JUGAR', 'url': "https://beacons.ai/juegosonline"}],
+            [{'text': 'APRENDE A JUGAR', 'url': "https://t.me/betvibes25/83"}],
+            [{'text': 'CÓMO RECARGAR', 'url': "https://t.me/betvibes25/91"}],
+        ]
+    }
+    
+    da = {
+        'chat_id': CHANNEL_ID,
+        'caption': mensaje,
+        'parse_mode': 'HTML',
+        'reply_markup': json.dumps(contenido)
+    } 
+
+    
+
+    requests.post(URL, data=da, files=files)
+
+
+
+
+with requests.Session() as session:
+
+    session.headers = headers
+    session.params = params
+
+    print("(+) Iniciando", flush=True)
+
+
+
+    almacen = []
+    while True:
+
+        try:
+            response = session.get(
+                'https://api.casinoscores.com/svc-evolution-game-events/api/icefishing',
+            )
+
+
+            if response.status_code != 200:
+                print(f"Error en el status code, status {response.status_code}", flush=True)
+                time.sleep(10)
+                continue
+
+
+            
+            objeto = response.json()
+
+
+            for resultado in objeto:
+
+                ganancia = resultado['totalAmount']
+
+
+                fecha = resultado['data']['settledAt']
+
+                if fecha in almacen:
+                    break
+
+                fecha_utc = datetime.fromisoformat(fecha).astimezone(timezone.utc) # Cambio la zona horaria a UTC, pues es en el formato en el que la web me da la fecha
+
+                fecha_utc_8 = fecha_utc.astimezone(utc_minus_8)
+                
+                fecha_final = fecha_utc_8.strftime('%H:%M %p %d/%m/%Y %Z') # Ahora la convierto al horario local del ordenador y la formateo
+                
+     
+                multiplicador= resultado['data']['result']['wheelResult']['maxMultiplier'] # El cliente necesita que este valor sea mayor o igual a 100
+
+                apostadores = resultado['bettorsCount']
+
+                tipo_pez = resultado['data']['result']['wheelResult']['wheelSector']
+
+        
+                if multiplicador >= 100 and multiplicador <=250:
+
+                    if tipo_pez == "LilBlues":
+                        pez = 'Pez AZUL 🔵'
+                        imagen = 'azul'
+
+                    elif tipo_pez == "BigOranges":
+                        pez = 'Pez NARANJA 🟠'
+                        imagen = 'naranja'
+                    elif tipo_pez == "HugeReds":
+                        pez = 'Pez Rojo 🔴'
+                        imagen = 'rojo'
+       
+
+                    multiplicador = str(multiplicador) + "X"
+
+                    mensaje = (f"SUPER <b>{pez}</b> de <b>{multiplicador}</b>\n\n"
+                            f"➡️ <b>Apostadores:</b> {apostadores}\n\n"
+                            f"➡️ <b>Ganancia:</b> {ganancia} $\n\n"
+                            f"➡️ <b>Fecha:</b> {fecha_final}\n\n"
+                            "🔎 <a href='https://telegra.ph/Bot-Ice-Fishing-12-01'><b>Información</b> acerca del <b>bot</b></a>"
+                            )
+
+
+                    telegram(mensaje=mensaje, imagen=imagen)
+
+                elif multiplicador > 250:
+                    multiplicador = str(multiplicador) + "X"
+
+                    imagen = 'otro'
+
+                    mensaje = (f"SUPER PEZ de <b>{multiplicador}</b>\n\n"
+                            f"➡️ <b>Apostadores:</b> {apostadores}\n\n"
+                            f"➡️ <b>Ganancia:</b> {ganancia} $\n\n"
+                            f"➡️ <b>Fecha:</b> {fecha_final}\n\n"
+                            "🔎 <a href='https://telegra.ph/Bot-Ice-Fishing-12-01'><b>Información</b> acerca del <b>bot</b></a>"
+                            )
+                    
+             
+
+                    telegram(mensaje=mensaje, imagen=imagen)
+
+
+            almacen.clear()
+            for resultado in objeto:
+                fecha = resultado['data']['settledAt']
+
+                almacen.append(fecha)
+
+
+            time.sleep(60)
+
+        except Exception as e:
+            mensaje= traceback.format_exc()
+            
+            print(mensaje, flush=True)
+
+            time.sleep(10)
+
+
+
+# print(objeto['data']['result']['wheelResult'])
